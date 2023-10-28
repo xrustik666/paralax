279684b57a62798d76ce328cf38fece8b324efe6
Xrustik <shaliruna4@gmail.com>
2023-10-28 14:46:23 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let headerNav = document.querySelector("header .nav");
+let footerNav = document.querySelector("footer .nav");
+
+let upButton = document.querySelector(".scrollUp");
+
+let homeButton = document.querySelectorAll(".home");
+let welcomeInfo = document.querySelector(".welcome-info");
+
+let aboutButton = document.querySelectorAll(".about");
+let aboutInfo = document.querySelector(".about-info");
+
+let contactsButton = document.querySelectorAll(".contacts");
+let contactInfo = document.querySelector(".contact-info");
+
+
+window.addEventListener('scroll', function() {
+	/* Assign a value of y-axis scroll to the variable '--scrolling' */
+	document.documentElement.style.setProperty('--scrolling', scrollY + 'px');
+
+	/* Upper menu disappears, lower menu apperas when user scrolls the page */
+	if(window.scrollY > 80) {
+		headerNav.style.display = "none";
+		footerNav.style.display = "flex";
+	} else {
+		headerNav.style.display = "flex";
+		footerNav.style.display = "none";
+	}
+});
+
+/* Returns to the top */
+upButton.addEventListener('click', function() {
+	document.documentElement.scrollTop = 0;
+});
+
+/* Clicking upper and lower navigation bar buttons */
+for (let i = 0; i <= 1; i++) {
+	homeButton[i].addEventListener("click", function() {
+		contactInfo.style.display = "none";
+		welcomeInfo.style.display = "block";
+		aboutInfo.style.display = "none";
+	});
+	
+	aboutButton[i].addEventListener("click", function() {
+		contactInfo.style.display = "none";
+		welcomeInfo.style.display = "none";
+		aboutInfo.style.display = "block";
+	});
+	
+	contactsButton[i].addEventListener("click", function() {
+		contactInfo.style.display = "block";
+		welcomeInfo.style.display = "none";
+		aboutInfo.style.display = "none";
+	});
+}
+
